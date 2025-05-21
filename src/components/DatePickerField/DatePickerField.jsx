0eea5fafb89b6cc186c6ded64d00cf6ee96fcbb3
BulatovaTati <kohodatat@gmail.com>
2025-05-21 19:50:08 +0300

    Fix Calendar styles

--- src/components/DatePickerField/DatePickerField.jsx
+++ src/components/DatePickerField/DatePickerField.jsx
@@ -2,9 +2,23 @@ import { forwardRef } from 'react';
 
 import DatePicker from 'react-datepicker';
 import s from '../BookingForm/BookingForm.module.css';
+import css from './DatePickerField.module.css';
 
 const DatePickerField = forwardRef(({ value, onChange }, ref) => {
-    return <DatePicker selected={value} onChange={onChange} className={s.input} minDate={new Date()} dateFormat="d MMMM, yyyy" placeholderText="Booking date" ref={ref} />;
+    return (
+        <DatePicker
+            selected={value}
+            onChange={onChange}
+            className={s.input}
+            shouldCloseOnSelect={true}
+            wrapperClassName={css.datepickerWrapper}
+            minDate={new Date()}
+            formatWeekDay={day => day.toUpperCase().slice(0, 3)}
+            dateFormat="d MMMM, yyyy"
+            placeholderText="Booking date*"
+            ref={ref}
+        />
+    );
 });
 
 export default DatePickerField;
