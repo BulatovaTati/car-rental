3f4ee9c3dfb55e7a224ae08c651ff4ce037c2f0e
BulatovaTati <kohodatat@gmail.com>
2025-05-21 19:51:10 +0300

    Change of hover/focus /Change validation texts

--- src/validations/bookingValidationForm.js
+++ src/validations/bookingValidationForm.js
@@ -1,10 +1,10 @@
 import * as Yup from 'yup';
 
 export const bookingValidationSchema = Yup.object().shape({
-    name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+    name: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Name is required'),
     email: Yup.string()
         .email('Must be a valid email!')
-        .required('Required')
+        .required('Email is required')
         .matches(/^(?!.*\.ru$)(?=.*@)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Must be a valid email!'),
     bookingDate: Yup.date().required('Booking date is required'),
     comment: Yup.string().max(200, 'Too Long!'),
